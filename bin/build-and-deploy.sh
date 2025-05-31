d3a76fe7b463a5b34f450291c85f90d0a5e594b3
Andy Gorman <hi@gorman.zone>
2025-05-31 14:26:49 -0400

    WIP - bash scripts

---
+++ bin/build-and-deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Author: Andy Gorman
+
+set -e
+
+pushd $(dirname "${BASH_SOURCE[0]}")/.. &> /dev/null
+
+
+popd &> /dev/null
---
+++ bin/lint-all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Author: Andy Gorman
+
+set -e
+
+pushd $(dirname "${BASH_SOURCE[0]}")/.. &> /dev/null
+
+
+
+
+popd &> /dev/null

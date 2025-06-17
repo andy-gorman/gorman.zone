bbbc78ccbd8726147524d246e31f176aeee1daa3
Andy Gorman <hi@gorman.zone>
2025-06-17 19:38:23 -0400

    Simplify logic

--- api/garmin/parser.go
+++ api/garmin/parser.go
@@ -2,8 +2,6 @@ package garmin
 
 import (
 	"errors"
-	"log/slog"
-	"net/http"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -26,30 +24,4 @@ func ParseLivetrackLinkFromEmail(htmlEmailBody string) (string, error) {
 	}
 
 	return "", errors.New("no links found in document")
-}
-
-func IsLivetrackLinkActive(url string) bool {
-	res, err := http.Get(url)
-	if err != nil {
-		slog.Error("Unable to fetch livetrack url", "error", err.Error())
-		return false
-	}
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		return false
-	}
-
-	doc, err := html.Parse(res.Body)
-	if err != nil {
-		slog.Error("Unable to parse response", "error", err.Error())
-	}
-
-	for n := range doc.Descendants() {
-		if n.Type == html.TextNode {
-			if n.Data == "The LiveTrack session you are looking for has ended." {
-				return false
-			}
-		}
-	}
-	return true
 }
--- api/main.go
+++ api/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"io"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/andy-gorman/gorman.zone/api/fastmail"
@@ -29,29 +33,44 @@ func main() {
 		Client: client,
 	}
 
+	server := &http.Server{
+		Addr: ":3333",
+	}
+
 	env.updateLivetrackUrl()
 	done := env.setupTimer()
+	shutdownChan := make(chan bool, 1)
 
 	http.HandleFunc("/garmin-live-track", env.handler())
 
-	err := http.ListenAndServe(":3333", nil)
-	if errors.Is(err, http.ErrServerClosed) {
-		slog.Info("server closed\n")
+	go func() {
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("HTTP server error:", "error", err)
+		}
 
-	} else if err != nil {
-		slog.Error("error starting server", "error", err.Error())
-		os.Exit(1)
+		slog.Info("Stopped serving new connections.")
+		done <- true
+		shutdownChan <- true
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
 	}
-	done <- true
+
+	<-shutdownChan
+	slog.Info("Graceful shutdown complete")
 }
 
 func (e *Env) handler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if garmin.IsLivetrackLinkActive(*e.LivetrackUrl) {
-			io.WriteString(w, *e.LivetrackUrl)
-		} else {
-			io.WriteString(w, "")
-		}
+		io.WriteString(w, *e.LivetrackUrl)
 	}
 }
 
@@ -67,7 +86,6 @@ func (e *Env) updateLivetrackUrl() {
 		return
 	}
 	e.LivetrackUrl = &link
-	slog.Info(*e.LivetrackUrl)
 }
 
 func (e *Env) setupTimer() chan bool {
